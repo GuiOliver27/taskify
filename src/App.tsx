d65e862ff59853b1bcd018a62c8aaa043e51c1ff
GuiOliver27 <guilherme_oliveira27@outlook.com>
2022-05-28 19:43:02 -0400

    Project updates and changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,13 @@
 import React from 'react';
 import './App.css';
 
-let name: any
-let age: number | string
-let isStudent: boolean
-let hobbies: string[]
-let role: [number, string]
-
-let printName: (name: string) => never
-
-/* type Person = {
-  name: string
-  age?: number
-};
-
- let person: Person = {
-  name: "Guilherme",
-};
-
-let lotsOfPeople: Person[]
-
-let personName: unknown */
-
-interface Person{
-  name: string
-  age?: number
-};
-
-type X = {
-  
-}
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <span className="heading">Taskify</span>
+      <InputFeild />
     </div>
   );
-}
+};
 
 export default App;
