3d9c7bd1439109606523193ac3d55b05180e134a
GuiOliver27 <guilherme_oliveira27@outlook.com>
2022-05-27 23:21:49 -0400

    This is a task manager app,  with some new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: any
+let age: number | string
+let isStudent: boolean
+let hobbies: string[]
+let role: [number, string]
+
+let printName: (name: string) => never
+
+/* type Person = {
+  name: string
+  age?: number
+};
+
+ let person: Person = {
+  name: "Guilherme",
+};
+
+let lotsOfPeople: Person[]
+
+let personName: unknown */
+
+interface Person{
+  name: string
+  age?: number
+};
+
+type X = {
+  
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
